35bedb47b07637f6463b89356a0cf7d0f08ec537
Nithin Deepkumar <nithindeepkumar@gmail.com>
2020-10-04 13:52:30 +0530

    add small change to PW  reset

--- app.js
+++ app.js
@@ -681,7 +681,7 @@ app.post('/forgot', function(req, res, next) {
 				let mailOptions = {
 					to: user.email,
 					from: 'nithinsblogapp229@gmail.com',
-					subject: 'Node.js Password Reset',
+					subject: 'The Food Blog Password Reset',
 					text:
 						'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
 						'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
